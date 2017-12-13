0e5361d6add5d075cd80c4f400951d01711d31eb
hebidu <hebiduhebi@126.com>
2017-12-13 15:39:06 +0800

    【更新】修复bug

--- nmp/init.sh
+++ nmp/init.sh
@@ -1,13 +1,16 @@
 
 
 # 二级目录
-mkdir /home/wdg/dnmp/etc & mkdir /home/wdg/dnmp/www & mkdir /home/wdg/dnmp/data
+mkdir /home/wdg/dnmp/etc && mkdir /home/wdg/dnmp/www && mkdir /home/wdg/dnmp/data
 
 # 三级目录
-mkdir /home/wdg/dnmp/data/mysql & mkdir /home/wdg/dnmp/etc/php & mkdir /home/wdg/dnmp/etc/nginx & mkdir /home/wdg/dnmp/etc/ssl & mkdir /home/wdg/dnmp/etc/mysql
+mkdir /home/wdg/dnmp/data/mysql && mkdir /home/wdg/dnmp/etc/php \
+ && mkdir /home/wdg/dnmp/etc/nginx && mkdir /home/wdg/dnmp/etc/ssl \
+  && mkdir /home/wdg/dnmp/etc/mysql
 
 # 复制
 
-cp -R ./nginx /home/wdg/dnmp/etc/ & cp -R ./www /home/wdg/dnmp/
+cp -R ./nginx /home/wdg/dnmp/etc/ && \
+cp -R ./www /home/wdg/dnmp/
 
 # TODO install docker , install docker-compose
