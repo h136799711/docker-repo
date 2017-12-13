8253caa0059e71bfe646fb1df00ce6183cb8f09d
hebidu <hebiduhebi@126.com>
2017-12-13 15:17:13 +0800

    【增加】add kms

--- nmp/init.sh
+++ nmp/init.sh
@@ -1,13 +1,13 @@
 
 
 # 二级目录
-mkdir /home/dnmp/etc & mkdir /home/dnmp/www & mkdir /home/dnmp/data
+mkdir /home/wdg/dnmp/etc & mkdir /home/wdg/dnmp/www & mkdir /home/wdg/dnmp/data
 
 # 三级目录
-mkdir /home/dnmp/data/mysql & mkdir /home/dnmp/etc/php & mkdir /home/dnmp/etc/nginx & mkdir /home/dnmp/etc/ssl & mkdir /home/dnmp/etc/mysql
+mkdir /home/wdg/dnmp/data/mysql & mkdir /home/wdg/dnmp/etc/php & mkdir /home/wdg/dnmp/etc/nginx & mkdir /home/wdg/dnmp/etc/ssl & mkdir /home/wdg/dnmp/etc/mysql
 
 # 复制
 
-cp -R ./nginx /home/dnmp/etc/ & cp -R ./www /home/dnmp/
+cp -R ./nginx /home/wdg/dnmp/etc/ & cp -R ./www /home/wdg/dnmp/
 
 # TODO install docker , install docker-compose
